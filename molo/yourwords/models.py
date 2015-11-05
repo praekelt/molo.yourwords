a3783351b40dbc8893bb12eab13b31f719bb092b
codiebeulaine <codiebeulaine@gmail.com>
2015-11-05 14:38:50 +0200

    taking out status choices

--- molo/yourwords/models.py
+++ molo/yourwords/models.py
@@ -5,13 +5,3 @@ class competition(models.Model):
     name = models.CharField(max_length=128)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    COMPETITION_STATUS_CHOICES = (
-        ('U', 'Unpublished'),
-        ('P', 'Published')
-    )
-    competition_status = models.CharField(
-        max_length=2,
-        choices=COMPETITION_STATUS_CHOICES,
-        blank=True,
-        null=True,
-    )
