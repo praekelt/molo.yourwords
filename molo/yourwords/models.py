9fd7ed55fd59bf9531a6ef9ae862d96c8c9c78ca
codiebeulaine <codiebeulaine@gmail.com>
2015-11-05 15:00:34 +0200

    adding competition entry model

--- molo/yourwords/models.py
+++ molo/yourwords/models.py
@@ -5,3 +5,20 @@ class competition(models.Model):
     name = models.CharField(max_length=128)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
+
+
+class competition_entry(models.Model):
+    story_name = models.CharField(max_length=128)
+    story_text = models.TextField()
+    TERMS_OR_CONDITIONS_CHOICES = (
+        ('Yes', 'Yes'),
+        ('No', 'No')
+    )
+
+    terms_or_conditions_approved = models.CharField(
+        choices=TERMS_OR_CONDITIONS_CHOICES)
+    HIDE_REAL_NAME_CHOICES = (
+        ('Yes', 'Yes'),
+        ('No', 'No')
+    )
+    hide_real_name = models.charfield(choices=HIDE_REAL_NAME_CHOICES)
