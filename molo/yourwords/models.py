0b896a9212dbe39a3e612dc258e268dcf8a426c1
codiebeulaine <codiebeulaine@gmail.com>
2015-11-06 10:30:15 +0200

    changed models

--- molo/yourwords/models.py
+++ molo/yourwords/models.py
@@ -1,23 +1,24 @@
 from wagtail.wagtailcore.models import Page
-from django.db import models
+
 
 class competition(Page):
     name = Page.CharField(max_length=128)
     startDate = Page.DateTimeField(null=True, blank=True)
     endDate = Page.DateTimeField(null=True, blank=True)
 
-class competition_entry(models.Model):
-    story_name = models.CharField(max_length=128)
-    story_text = models.TextField()
+
+class competition_entry(Page):
+    story_name = Page.CharField(max_length=128)
+    story_text = Page.TextField()
     TERMS_OR_CONDITIONS_CHOICES = (
         ('Yes', 'Yes'),
         ('No', 'No')
     )
 
-    terms_or_conditions_approved = models.CharField(
+    terms_or_conditions_approved = Page.CharField(
         choices=TERMS_OR_CONDITIONS_CHOICES)
     HIDE_REAL_NAME_CHOICES = (
         ('Yes', 'Yes'),
         ('No', 'No')
     )
-    hide_real_name = models.charfield(choices=HIDE_REAL_NAME_CHOICES)
+    hide_real_name = Page.charfield(choices=HIDE_REAL_NAME_CHOICES)
