b241bbb935cc1b3d2317aa6f9f2c546257db367a
codiebeulaine <codiebeulaine@gmail.com>
2015-11-06 13:09:04 +0200

    removing choices

--- molo/yourwords/models.py
+++ molo/yourwords/models.py
@@ -11,15 +11,5 @@ class Competition(Page):
 class CompetitionEntry(Page):
     story_name = Page.CharField(max_length=128)
     story_text = Page.TextField()
-    TERMS_OR_CONDITIONS_CHOICES = (
-        ('Yes', 'Yes'),
-        ('No', 'No')
-    )
-
-    terms_or_conditions_approved = Page.BooleanField(
-        choices=TERMS_OR_CONDITIONS_CHOICES)
-    HIDE_REAL_NAME_CHOICES = (
-        ('Yes', 'Yes'),
-        ('No', 'No')
-    )
-    hide_real_name = Page.BooleanField(choices=HIDE_REAL_NAME_CHOICES)
+    terms_or_conditions_approved = Page.BooleanField()
+    hide_real_name = Page.BooleanField()
