0446d272a481a6a68b0ca7a056623322d4c41440
Saeed Marzban <Saeed.mb8@gmail.com>
2015-11-10 10:53:43 +0200

    Added terms and conditions and thank you page to models

--- molo/yourwords/migrations/0001_initial.py
+++ molo/yourwords/migrations/0001_initial.py
@@ -12,11 +12,32 @@ import wagtail.wagtailcore.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('core', '0018_auto_20150925_1554'),
         ('wagtailimages', '0006_add_verbose_names'),
         ('wagtailcore', '0001_squashed_0016_change_page_url_path_to_text_field'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='TermsAndConditions',
+            fields=[
+                ('articlepage_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.ArticlePage')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('core.articlepage',),
+        ),
+        migrations.CreateModel(
+            name='ThankYou',
+            fields=[
+                ('articlepage_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.ArticlePage')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('core.articlepage',),
+        ),
         migrations.CreateModel(
             name='YourWordsCompetition',
             fields=[
@@ -25,6 +46,7 @@ class Migration(migrations.Migration):
                 ('content', wagtail.wagtailcore.fields.StreamField([(b'heading', wagtail.wagtailcore.blocks.CharBlock(classname=b'full title')), (b'paragraph', molo.core.blocks.MarkDownBlock()), (b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'list', wagtail.wagtailcore.blocks.ListBlock(wagtail.wagtailcore.blocks.CharBlock(label=b'Item'))), (b'numbered_list', wagtail.wagtailcore.blocks.ListBlock(wagtail.wagtailcore.blocks.CharBlock(label=b'Item'))), (b'page', wagtail.wagtailcore.blocks.PageChooserBlock())], null=True, blank=True)),
                 ('start_date', models.DateTimeField(null=True, blank=True)),
                 ('end_date', models.DateTimeField(null=True, blank=True)),
+                ('extra_style_hints', models.TextField(default=b'', help_text='Styling options that can be applied to this section and all its descendants', null=True, blank=True)),
                 ('image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
             ],
             options={
@@ -45,10 +67,5 @@ class Migration(migrations.Migration):
                 'abstract': False,
             },
             bases=('wagtailcore.page',),
-        ),
-        migrations.AddField(
-            model_name='yourwordscompetition',
-            name='terms_and_conditions_link_page',
-            field=models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailcore.Page', help_text=b'Link to terms and conditions page', null=True),
         ),
     ]
