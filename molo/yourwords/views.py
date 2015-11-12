656bcf17acd35990c950eb99d30b03ba3b501660
Saeed Marzban <Saeed.mb8@gmail.com>
2015-11-12 09:29:13 +0200

    added more to views

--- molo/yourwords/views.py
+++ molo/yourwords/views.py
@@ -1,7 +1,18 @@
 from django.views.generic.edit import CreateView
 from molo.yourwords.forms import CompetitionEntryForm
+from django.views.generic.edit import FormView
 
 
 class CompetitionEntry(CreateView):
     form_class = CompetitionEntryForm
-    template_name = 'yourwords/Your_words_competition.html'
+    template_name = 'yourwords/your_words_competition.html'
+
+
+class CompetitionEntryView(FormView):
+    template_name = 'thank_you.html'
+    form_class = CompetitionEntryForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+
+        return super(CompetitionEntryView, self).form_valid(form)
