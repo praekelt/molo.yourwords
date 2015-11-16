01890ac4d299a156ca8181778bb62b6bb4982e4a
Saeed Marzban <Saeed.mb8@gmail.com>
2015-11-16 17:31:22 +0200

    added error message

--- molo/yourwords/forms.py
+++ molo/yourwords/forms.py
@@ -4,6 +4,8 @@ from molo.yourwords.models import YourWordsCompetitionEntry
 
 
 class CompetitionEntryForm(forms.ModelForm):
+    terms_or_conditions_approved = forms.BooleanField(required=True)
+
     class Meta:
         model = YourWordsCompetitionEntry
         fields = ['story_name', 'story_text', 'terms_or_conditions_approved',
