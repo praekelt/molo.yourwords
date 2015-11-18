19b32c50bd7a1374151c04138768c540d0471552
Saeed Marzban <Saeed.mb8@gmail.com>
2015-11-18 15:01:59 +0200

    added competition tag

--- molo/yourwords/templatetags/competition_tag.py
+++ molo/yourwords/templatetags/competition_tag.py
@@ -1,6 +1,4 @@
 from django import template
-from django.shortcuts import get_object_or_404
-from wagtail.wagtailcore.models import Page
 from copy import copy
 from molo.yourwords.models import YourWordsCompetition
 
@@ -8,12 +6,12 @@ register = template.Library()
 
 
 @register.inclusion_tag(
-    'yourwords/your_words_competition.html',
+    'yourwords/your_words_competition_tag.html',
     takes_context=True
 )
 def your_words_competition(context, page=None):
     context = copy(context)
     context.update({
-        'competition': YourWordsCompetition.objects.live().child_of(page)
+        'competitions': YourWordsCompetition.objects.live()
     })
     return context
