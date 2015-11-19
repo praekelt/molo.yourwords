c8f374101c5abd482ff1182215666e5c91e6a473
codiebeulaine <codiebeulaine@gmail.com>
2015-11-19 19:01:21 +0200

    made changes to model admin

--- molo/yourwords/admin.py
+++ molo/yourwords/admin.py
@@ -1,11 +1,13 @@
 import json
 
-from molo.core.models import ArticlePage, LanguagePage
-from molo.yourwords.models import (
-    YourWordsCompetitionEntry, YourWordsCompetition)
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import truncatechars
+from django import forms
+
+from molo.core.models import ArticlePage, LanguagePage
+from molo.yourwords.models import (
+    YourWordsCompetitionEntry, YourWordsCompetition)
 
 
 def convert_to_article(model_admin, request, entry):
@@ -21,13 +23,25 @@ def convert_to_article(model_admin, request, entry):
 convert_to_article.short_description = "Convert competition entry to article"
 
 
+class YourWordsCompetitionEntryForm(forms.ModelForm):
+
+    class Meta:
+        model = YourWordsCompetitionEntry
+        fields = ['story_name', 'story_text', 'user', 'hide_real_name',
+                  'is_read', 'is_shortlisted', 'is_winner']
+
+
 class YourWordsCompetitionEntryAdmin(admin.ModelAdmin):
     list_display = ['story_name', 'truncate_text', 'user', 'hide_real_name',
                     'submission_date', 'is_read', 'is_shortlisted',
                     'is_winner']
     list_filter = ['competition__slug', 'is_read', 'is_shortlisted',
-                   'is_winner']
+                   'is_winner', 'submission_date']
     list_editable = ['is_read', 'is_shortlisted', 'is_winner']
+    date_hierarchy = 'submission_date'
+    form = YourWordsCompetitionEntryForm
+    readonly_fields = ('competition', 'story_name', 'story_text', 'user',
+                       'hide_real_name', 'submission_date')
 
     def truncate_text(self, obj, *args, **kwargs):
         return truncatechars(obj.story_text, 30)
@@ -36,9 +50,20 @@ class YourWordsCompetitionEntryAdmin(admin.ModelAdmin):
 
 
 class YourWordsCompetitionAdmin(admin.ModelAdmin):
-    list_display = ['entries', 'start_date', 'end_date', 'live']
+    list_display = ['entries', 'start_date', 'end_date', 'status',
+                    'number_of_entries']
     list_filter = ['title', 'start_date', 'end_date']
     search_fields = ['title', 'content', 'description']
+    date_hierarchy = 'start_date'
+
+    def number_of_entries(self, obj, *args, **kwargs):
+        return YourWordsCompetitionEntry.objects.filter(
+            competition__slug=obj.slug).count()
+
+    def status(self, obj, *args, **kwargs):
+        if obj.live:
+            return 'First published on ' + str(obj.first_published_at)
+        return 'Unpublished'
 
     def entries(self, obj, *args, **kwargs):
         url = reverse('admin:yourwords_yourwordscompetitionentry_changelist')
