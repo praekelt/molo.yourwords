9ed04b1cb89bdb6f67bf386a419ddab8556607ac
Milton Madanda <madandat@gmail.com>
2015-11-19 14:26:40 +0200

    filter by competition

--- molo/yourwords/admin.py
+++ molo/yourwords/admin.py
@@ -1,8 +1,10 @@
 import json
 
 from molo.core.models import ArticlePage, LanguagePage
-from molo.yourwords.models import YourWordsCompetitionEntry, YourWordsCompetition
+from molo.yourwords.models import (
+    YourWordsCompetitionEntry, YourWordsCompetition)
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 
 def convert_to_article(model_admin, request, entry):
@@ -20,13 +22,21 @@ convert_to_article.short_description = "Convert competition entry to article"
 
 class YourWordsCompetitionEntryAdmin(admin.ModelAdmin):
     list_display = ['competition', 'story_name', 'user', 'story_text']
+    list_filter = ['competition__slug']
     actions = [convert_to_article]
 
 
 class YourWordsCompetitionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'start_date', 'end_date', 'live']
-    list_filter = ['title', 'start_date', 'end_date']
+    list_display = ['entries', 'start_date', 'end_date', 'live']
+    list_filter = ['title', 'start_date', 'end_date', 'description']
     search_fields = ['title', 'content', 'description']
+
+    def entries(self, obj, *args, **kwargs):
+        url = reverse('admin:yourwords_yourwordscompetitionentry_changelist')
+        return '<a href="%s?competition__slug=%s">%s</a>' % (
+            url, obj.slug, obj)
+    entries.allow_tags = True
+    entries.short_description = 'Title'
 
 
 admin.site.register(YourWordsCompetitionEntry, YourWordsCompetitionEntryAdmin)
