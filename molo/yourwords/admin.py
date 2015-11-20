e94ddfc3b6634e79b99a4fe55ed2f27fb6305df8
codiebeulaine <codiebeulaine@gmail.com>
2015-11-20 13:19:07 +0200

    added test

--- molo/yourwords/admin.py
+++ molo/yourwords/admin.py
@@ -26,6 +26,7 @@ def download_as_csv(YourWordsCompetitionEntryAdmin, request, queryset):
     writer.writerow(field_names)
     for obj in queryset:
         writer.writerow([getattr(obj, field) for field in field_names])
+    print response
     return response
 download_as_csv.short_description = "Download selected as csv"
 
