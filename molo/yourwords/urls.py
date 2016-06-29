090185b18bb9d7dd7f766ad68a9eed650acb04c0
Richard Landers <infolog>
2016-06-29 16:11:58 +0200

    removed export url

--- molo/yourwords/urls.py
+++ molo/yourwords/urls.py
@@ -1,5 +1,4 @@
 from molo.yourwords import views
-from molo.yourwords.wagtail_hooks import YourWordEntriesExport
 
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
@@ -16,7 +15,4 @@ urlpatterns = patterns(
         r'^thankyou/(?P<slug>[\w-]+)/$',
         login_required(views.ThankYouView.as_view()),
         name='thank_you'),
-
-    url(r'admin/entries_export/$', (YourWordEntriesExport.as_view()),
-        name='yourword_entries_export'),
 )
