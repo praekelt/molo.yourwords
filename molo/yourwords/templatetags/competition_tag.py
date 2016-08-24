430e88dd697e1b0dcca9bef8b27479b1360a5957
Saeed Marzban <Saeed.mb8@gmail.com>
2016-08-24 16:13:10 +0200

    fixed travis

--- molo/yourwords/templatetags/competition_tag.py
+++ molo/yourwords/templatetags/competition_tag.py
@@ -2,7 +2,7 @@ from django import template
 from copy import copy
 from molo.yourwords.models import (YourWordsCompetition, ThankYou,
                                    YourWordsCompetitionIndexPage)
-from molo.core.core_tags import get_pages
+from molo.core.templatetags.core_tags import get_pages
 
 register = template.Library()
 
